828c0cbf1e6c7726a3bd2e2ac4a2fe21ad055a6d
troyan911 <troyan911@gmail.com>
2023-09-05 00:50:33 +0300

    lesson03 hw4

---
+++ src/lesson03/conditions.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang = "en";
+$name = "John";
+$message = null;
+
+
+if($lang === "ua") {
+    $message = "Pruvit";
+}
+else if($lang === "de") {
+    $message = "Hi";
+}
+$result = $message . " " . $name .PHP_EOL;
+echo $result . PHP_EOL;
+
+
+$message = match($lang) {
+  "ua" => "Pruvit",
+  "en" => "Hello",
+  default => "Hi"
+};
+
+echo $message;
+
+switch ($lang){
+    case "ua":
+        $message = "Pruvit";
+        break;
+}
