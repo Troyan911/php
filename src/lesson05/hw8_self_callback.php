7edb411b9907cca8faa1fc26a40a49d62e28bdee
troyan911 <troyan911@gmail.com>
2023-09-13 14:50:54 +0300

    hw8 fixes

--- src/lesson05/hw8_self_callback.php
+++ src/lesson05/hw8_self_callback.php
@@ -1,1 +1,19 @@
 <?php
+declare(strict_types=1);
+
+function multiply(int $arg1, int $arg2, ? closure $print_result = null): int
+{
+    $result = $arg1 * $arg2;
+    if ($print_result) {
+        $print_result($result);
+    }
+    return $result;
+}
+
+$print_result = function (int $result): void {
+    echo "Result of multiplying is $result" . PHP_EOL;
+};
+
+echo multiply(10, 20) . PHP_EOL;
+echo multiply(10, 20, null) . PHP_EOL;
+echo multiply(10, 20, $print_result) . PHP_EOL;
