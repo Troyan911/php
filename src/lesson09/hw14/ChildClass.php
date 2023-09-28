c3d573c11827ebf92f2d52cf66fcd948706f8b1c
troyan911 <troyan911@gmail.com>
2023-09-28 19:10:38 +0300

    hw14

--- src/lesson09/hw14/ChildClass.php
+++ src/lesson09/hw14/ChildClass.php
@@ -1,6 +1,10 @@
 <?php
+declare(strict_types=1);
 
 class ChildClass extends ParentClass {
+    /**
+     * @return void
+     */
     public function print(): void {
         echo strtoupper($this->text) . PHP_EOL;
     }
--- src/lesson09/hw14/ParentClass.php
+++ src/lesson09/hw14/ParentClass.php
@@ -1,8 +1,11 @@
 <?php
-
+declare(strict_types=1);
 class ParentClass {
     protected $text = "some text";
 
+    /**
+     * @return void
+     */
     public function print() : void {
         echo ucfirst($this->text) . PHP_EOL;
     }
